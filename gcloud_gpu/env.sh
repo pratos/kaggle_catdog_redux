57f33d8a0ff69318924ad60cada6fb398806695f
Prathamesh <pratos9@live.com>
2017-06-01 01:22:32 +0530

    Add files via upload

---
+++ gcloud_gpu/env.sh
@@ -0,0 +1,3 @@
+echo 'export CUDA_HOME=/usr/local/cuda' >> ~/.bashrc
+echo 'export PATH=$PATH:$CUDA_HOME/bin' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_HOME/lib64'
---
+++ gcloud_gpu/pip.sh
@@ -0,0 +1,12 @@
+pip install tensorflow-gpu
+pip install keras
+pip install lasagne
+pip install matplotlib
+pip install pandas
+pip install jupyter
+pip install seaborn
+pip install tqdm
+pip install scikit-learn
+pip instal scikit-image
+conda install -c menpo opencv3
+pip install google-cloud
